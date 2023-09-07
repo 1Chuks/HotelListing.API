de5ad485d3c439cffc731ab0bb1c04b732fc954f
1Chuks <chukwuka.robert@gmail.com>
2023-09-08 00:13:48 +0100

    REST API Development Best Practices. Automapper and Repositories

---
+++ HotelListing.API/Models/Country/BaseCountryDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.Models.Country
+{
+    //abstract class means you can't initialize or instantiate them as stand alone objects. They are usually used for inheritance purposes.
+    public abstract class BaseCountryDto
+    {
+        [Required]
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
---
+++ HotelListing.API/Models/Country/UpdateCountryCto.cs
@@ -0,0 +1,7 @@
+﻿namespace HotelListing.API.Models.Country
+{
+    public class UpdateCountryCto : BaseCountryDto
+    {
+        public int Id { get; set; }
+    }
+}
